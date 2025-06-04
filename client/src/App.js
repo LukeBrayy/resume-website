9d8914d0386d39110034271bc4833c168ace973d
luke.bray <luke.bray@mrl.com.au>
2025-06-04 15:56:52 +0800

    Revert "meeting notes page"
    This reverts commit f7ea9abfa8c872e81b2711b073007d0b72ba6943.

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import Chat2 from './components/Chat2';
 import SecretSanta from './components/SecretSanta';
 import Rates from './components/Rates';
 import TimeseriesViewer from './components/TimeseriesViewer';
-import TrailFinder from './components/TrailFinder/TrailFinder';
-import MeetingNotes from './components/MeetingNotes/MeetingNotes';
 
 const App = () => {
   return (
@@ -19,9 +17,6 @@ const App = () => {
         <Route path="/Chat2" element={<Chat2 />} />
         <Route path="/rates" element={<Rates />} />
         <Route path="/charter" element={<TimeseriesViewer />} />
-        <Route path="/trailfinder" element={<TrailFinder />} />
-        <Route path="/meeting_notes" element={<MeetingNotes />} />
-        <Route path="*" element={<h1>404 Not Found, what's your problem bro ?</h1>} />
       </Routes>
     </Router>
   );
